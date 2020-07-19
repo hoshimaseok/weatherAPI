cba540e89c3ef417fbd4d04db25be3252817c4da
Arushi Jain <arushijain1479@gmail.com>
2020-07-19 16:55:20 +0530

    feat: Added post route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 require('dotenv').config();
 
+
 const apiKey = process.env.API_KEY;
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +17,25 @@ app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null});
 })
 
+app.post('/', function (req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  request(url, function (err, response, body) {
+      if(err){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+        console.log(err);
+      } else {
+        let weather = JSON.parse(body);
+        console.log(weather);
+        if(weather.main == undefined){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+          res.render('index', {weather: weatherText, error: null});
+        }
+      }
+    });
+  })
 
 app.listen(3000, function () {
   console.log('Listening on port 3000')
