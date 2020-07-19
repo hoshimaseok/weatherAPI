08e139f2c8ded7f0503a7780842594bc3ddf11ca
Arushi Jain <arushijain1479@gmail.com>
2020-07-19 16:26:21 +0530

    feat: Added get route

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const bodyParser = require('body-parser');
 const request = require('request');
 require('dotenv').config();
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
 
 
 app.listen(3000, function () {
